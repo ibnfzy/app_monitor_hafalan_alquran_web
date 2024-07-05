f62313f7ef12343bebce432db2e88fc6d1fdadf0
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2024-07-05 19:17:03 +0800

    home

--- app/Views/web/home.php
+++ app/Views/web/home.php
@@ -4,9 +4,9 @@
 <!-- CODE HERE -->
 <div class="owl-carousel owl-theme container-fluid">
   <?php foreach ($data as $item) : ?>
-  <div class="item">
-    <img src="/uploads/<?= $item['gambar'] ?>" class="" style="width: 90%;" alt="" srcset="">
-  </div>
+    <div class="item">
+      <img src="/uploads/<?= $item['gambar'] ?>" class="" style="width: 90%;" alt="" srcset="">
+    </div>
   <?php endforeach ?>
 </div>
 <?= $this->endSection(); ?>
